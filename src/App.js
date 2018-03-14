451cc994e7f3828e7fa73c5331eab92dde6615ff
Junard <jtoclarit@gmail.com>
2018-03-14 21:42:51 +0800

    Add Button component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from "./components/Header/Header";
+import Button from "./components/Button/Button";
 
 class App extends Component {
   render() {
@@ -7,6 +8,58 @@ class App extends Component {
       <div className="App">
           {/* You can use light, dark and transparent style  */}
           <Header headerStyle="dark" />
+          <div className="main-container">
+            <section>
+              <div className="container">
+                <h1>Buttons</h1>
+                <Button
+                  link="#"
+                  size="sm"
+                  rounded
+                  style={{
+                    color: 'white',
+                    backgroundColor: 'red',
+                    borderColor: '#000',
+                    hoverColor: '#000',
+                    hoverBgColor: 'gray',
+                    hoverBorderColor: "green"
+                  }}>Button</Button>
+
+                  <Button
+                    link="#"
+                    rounded
+                    style={{
+                      color: 'white',
+                      backgroundColor: 'blue',
+                      borderColor: 'yellow',
+                      hoverColor: 'blue',
+                      hoverBgColor: 'white',
+                      hoverBorderColor: "blue"
+                    }}>Button
+                  </Button>
+
+                  <Button
+                      link="#"
+                      style={{}}
+                    >Button
+                  </Button>
+
+                  <Button
+                      link="#"
+                      size="lg"
+                      style={{}}
+                    >Button
+                  </Button>
+
+                    <Button
+                      link="#"
+                      icon
+                      style={{}}
+                    ><i className="ti-arrow-up"></i>
+                  </Button>
+              </div>
+            </section>
+          </div>
       </div>
     );
   }
---
+++ src/components/Button/Button.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import './Button.css';
+
+class Button extends Component {
+
+  constructor(props) {
+    super(props);
+
+    const buttonStyle = this.props.style;
+
+    this.state = {
+      style: {
+        color: buttonStyle.color ? buttonStyle.color : '#47b475',
+        backgroundColor: buttonStyle.backgroundColor ? buttonStyle.backgroundColor : 'transparent',
+        borderColor: buttonStyle.borderColor ? buttonStyle.borderColor : '#47b475'
+      },
+      normalStyle : {},
+      onHoverdStyle : {
+        color: buttonStyle.hoverColor ? buttonStyle.hoverColor : '#fff',
+        backgroundColor: buttonStyle.hoverBgColor ? buttonStyle.hoverBgColor : '#47b475',
+        borderColor: buttonStyle.hoverBorderColor ? buttonStyle.hoverBorderColor : '#47b475',
+      },
+      hovered: false,
+      className: ['btn']
+    }
+
+
+		this._toggleHover = this._toggleHover.bind(this);
+
+
+  }
+
+  componentDidMount() {
+
+		let normalStyle = this.state.style;
+
+		this.setState({
+			normalStyle,
+      className: this._addButtonClass(this.state.className)
+		});
+
+	}
+
+  _addButtonClass(className) {
+    
+    //Size of the Button
+    if(this.props.size) {
+      className.push("btn-"+this.props.size);
+    }
+
+    //Rounded Button
+    if(this.props.rounded) {
+      className.push("btn-rounded");
+    }
+
+    //Icon Button
+    if(this.props.icon) {
+      className.push("btn-icon");
+    }
+
+    return className
+
+  }
+
+	_toggleHover() {
+
+		let hovered = this.state.hovered;
+		let style = {};
+
+		if(hovered) {
+			style = this.state.normalStyle;
+		} else {
+			style = this.state.onHoverdStyle;
+		}
+
+		// toggle hovered state
+		hovered = !hovered;
+		this.setState({
+			hovered,
+			style
+		});
+
+	}
+
+  render() {
+
+    return (
+      <a
+        href={this.props.link}
+        className={this.state.className.join(" ")}
+        style={Object.assign(this.state.style)}
+        onMouseOver={this._toggleHover}
+        onMouseOut={this._toggleHover}
+      >
+        {this.props.children}
+      </a>
+    )
+  }
+
+}
+
+export default Button
